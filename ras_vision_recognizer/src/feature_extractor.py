fed4962e2b4be2295394e5f1329be7d04a760213
Fabian Schilling <fabsch@kth.se>
2015-11-05 01:14:07 +0100

    svm learned

--- ras_vision_recognizer/src/feature_extractor.py
+++ ras_vision_recognizer/src/feature_extractor.py
@@ -12,6 +12,7 @@ from sklearn import preprocessing
 # ROS imports
 import rospy
 from sensor_msgs.msg import Image
+from ras_vision_recognizer.msg import Rect
 from cv_bridge import CvBridge
 
 
@@ -22,62 +23,62 @@ class FeatureExtractor:
         self.node_name = 'feature_extractor'
 
         cv2.namedWindow('original', cv2.WINDOW_NORMAL)
-        cv2.createTrackbar('blur', 'original', 3, 10, self.cb)
+        cv2.createTrackbar('blur', 'original', 0, 20, self.cb)
 
-        cv2.namedWindow('morphed', cv2.WINDOW_NORMAL)
-
-        cv2.namedWindow('edges', cv2.WINDOW_NORMAL)
-        cv2.createTrackbar('thresh1', 'edges', 0, 255, self.cb)
-        cv2.createTrackbar('thresh2', 'edges', 0, 255, self.cb)
+        cv2.namedWindow('canny', cv2.WINDOW_NORMAL)
+        cv2.createTrackbar('lower', 'canny', 0, 255, self.cb)
+        cv2.createTrackbar('upper', 'canny', 0, 255, self.cb)
 
         self.bridge = CvBridge()
 
         rospy.init_node(self.node_name, anonymous=True)
 
-        self.subscriber = rospy.Subscriber('vision/object_image', Image, self.image_callback, queue_size=1)
+        self.image = None
+
+        self.count = 0
+
+        self.subscriber = rospy.Subscriber('/camera/rgb/image_raw', Image, self.color_callback, queue_size=1)
+        self.subscriber = rospy.Subscriber('vision/object_rect', Rect, self.object_callback, queue_size=1)
 
         #self.publisher = rospy.Publisher('vision/object_rect', Rect, queue_size=1)
 
     def cb(self, x):
         pass
 
-    def image_callback(self, data):
+    def color_callback(self, data):
 
-        # Convert from ROS image to OpenCV image
-        image = self.bridge.imgmsg_to_cv2(data)
+        self.image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
 
-        blur = cv2.getTrackbarPos('blur', 'original')
+    def object_callback(self, d):
 
-        if blur > 0:
-            image = cv2.blur(image, (blur, blur))
+        # Return if no image available
+        if self.image is None:
+            return
+
+        # Crop the object
+        image = self.image[d.y:d.y + d.height, d.x:d.x + d.width]
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        image = cv2.blur(image, (3, 3))
 
         cv2.imshow('original', image)
 
-        #res = cv2.adaptiveThreshold(original, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 3)
+        mask = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 3, 2)
 
-        otsu_thresh, _ = cv2.threshold(image, 0, 255, type=cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, (5, 5))
 
-        edges = cv2.Canny(image, otsu_thresh * 0.5, otsu_thresh)
+        resized = cv2.resize(opening, (30, 30))
 
-        cv2.imshow('morphed', edges)
-
-        # thresh1 = cv2.getTrackbarPos('thresh1', 'edges')
-        # thresh2 = cv2.getTrackbarPos('thresh2', 'edges')
-        
-        # edges = cv2.Canny(res, thresh1, thresh2)
-
-        # cv2.imshow('edges', edges)
-
-        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5, 5))
-        # close = cv2.morphologyEx(resized,cv2.MORPH_CLOSE, kernel)
-        # div = np.float32(resized) / (close)
-        # res = np.uint8(cv2.normalize(div, div, 0, 255, cv2.NORM_MINMAX))
+        cv2.imshow('canny', resized)
 
         key = cv2.waitKey(1)
         
         if key == 10: # Return key pressed
-            print('Image captured')
-            cv2.imwrite('image.jpg', resized)
+            filename = 'images/image' + str(self.count + 1) + '.jpg'
+            cv2.imwrite(filename, resized)
+            print('Image saved: ' + filename)
+            self.count += 1
 
 def main():
     print('Running... Press CTRL-C to quit.')
