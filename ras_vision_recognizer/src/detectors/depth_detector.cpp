93a41a59c766f554c568bd6e269289c004743c1b
Fabian Schilling <fabsch@kth.se>
2015-11-06 20:07:50 +0100

    depth detector implemented in c++

---
+++ ras_vision_recognizer/src/detectors/depth_detector.cpp
@@ -0,0 +1,150 @@
+// C++
+#include <iostream>
+#include <vector>
+#include <string>
+
+// ROS
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+// ROS messages
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <ras_vision_recognizer/Rect.h>
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/photo/photo.hpp> // inpaint
+
+// Constants
+static const double MIN_RANGE = 0.0;
+static const double MAX_RANGE = 1.0;
+static const int PADX = 70;
+static const int PADY = 20;
+static const int EROSION = 20;
+static const int DILATION = 1;
+static const int MIN_SIZE = 4000;
+static const int MAX_SIZE = 20000;
+static const double MIN_ASPECT = 0.8;
+static const double MAX_ASPECT = 1.2;
+static const cv::Scalar color_black(0, 0, 0);
+
+// Global variables
+ros::Subscriber subscriber;
+ros::Publisher publisher;
+
+bool compare_contour_areas(std::vector<cv::Point> contour1, std::vector<cv::Point> contour2) {
+    return std::fabs(cv::contourArea(cv::Mat(contour1))) < std::fabs(cv::contourArea(cv::Mat(contour2)));
+}
+
+void depth_callback(const sensor_msgs::Image::ConstPtr& message) {
+
+    cv_bridge::CvImagePtr cv_image;
+
+    try {
+        cv_image = cv_bridge::toCvCopy(message, sensor_msgs::image_encodings::TYPE_32FC1);
+    } catch (cv_bridge::Exception& e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    // Get mask of NaN values
+    cv::Mat nan_mask = cv::Mat(cv_image->image != cv_image->image);
+
+    // Map range from 0 to 255 based on MIN_RANGE and MAX_RANGE
+    cv::Mat image(cv_image->image.rows, cv_image->image.cols, CV_8UC1);
+    for (int i = 0; i < cv_image->image.rows; i++) {
+
+        float* di = cv_image->image.ptr<float>(i);
+        char* ii = image.ptr<char>(i);
+
+        for (int j = 0; j < cv_image->image.cols; j++) {
+            ii[j] = (char) (255 * ((di[j] - MIN_RANGE) / (MAX_RANGE - MIN_RANGE)));
+        }
+    }
+
+    // Extract ROI without noise
+    cv::Rect roi = cv::Rect(PADX, PADY, image.cols - (2 * PADX), image.rows - (2 * PADY));
+    cv::Mat roi_image = image(roi);
+    cv::Mat roi_nan_mask = nan_mask(roi);
+    cv::rectangle(image, roi, color_black);
+    //cv::imshow("nan", roi_nan_mask);
+
+    // Inpainting
+    cv::Mat roi_inpainted;
+    cv::inpaint(roi_image, roi_nan_mask, roi_inpainted, 1.0, cv::INPAINT_TELEA);
+    //cv::imshow("inpainted", roi_inpainted);
+
+    // Adaptive Threshold (based on inpaint)
+    cv::Mat thresh;
+    int block_size = 3;
+    double param1 = 2.0;
+    cv::adaptiveThreshold(roi_inpainted, thresh, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, block_size, param1);
+    //cv::imshow("thresh", thresh);
+
+    // Erosion and dilation
+    cv::Mat erosion_kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(EROSION, EROSION));
+    cv::erode(thresh, thresh, erosion_kernel);
+
+    cv::Mat dilation_kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(DILATION, DILATION));
+    cv::erode(thresh, thresh, dilation_kernel);
+
+    // Find and draw contours
+    std::vector<std::vector<cv::Point> > contours;
+    cv::findContours(thresh, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+    //cv::drawContours(roi_image, contours, -1, black);
+
+    // Check if contours found
+    if (contours.size() > 1) {
+
+        // Sort contours by contour area
+        std::sort(contours.begin(), contours.end(), compare_contour_areas);
+
+        std::vector<cv::Point> object_contour = contours[contours.size() - 2];
+
+        cv::Rect bounding_rect = cv::boundingRect(cv::Mat(object_contour));
+
+        int size = bounding_rect.area();
+        double aspect = (double) bounding_rect.width / bounding_rect.height;
+
+        // Check if size and aspect ratio qualifies for object
+        if (size > MIN_SIZE && size < MAX_SIZE && aspect > MIN_ASPECT && aspect < MAX_ASPECT) {
+
+            // Draw a rectangle around the object
+            cv::rectangle(roi_inpainted, bounding_rect, color_black);
+
+            ras_vision_recognizer::Rect message;
+            message.x = bounding_rect.x;
+            message.y = bounding_rect.y;
+            message.width = bounding_rect.width;
+            message.height = bounding_rect.height;
+
+            publisher.publish(message);
+        }
+    }
+
+    cv::imshow("image", roi_inpainted);
+
+    if (int key = cv::waitKey(1) != -1) {
+        std::cout << key << " pressed." << std::endl;
+    }
+
+}
+
+int main(int argc, char ** argv) {
+
+    ros::init(argc, argv, "depth_detector");
+
+    ros::NodeHandle nh;
+
+    subscriber = nh.subscribe<sensor_msgs::Image>("/camera/depth/image", 1, depth_callback);
+
+    publisher = nh.advertise<ras_vision_recognizer::Rect>("/object/rect", 1);
+
+    ros::spin();
+
+    return 0;
+}
