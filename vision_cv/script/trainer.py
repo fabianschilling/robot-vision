0b7b5ebc68e31cf7b3a0e836ef5dc6dd01532446
Fabian Schilling <fabsch@kth.se>
2015-11-29 19:51:00 +0100

    new classifier works great

--- vision_cv/script/trainer.py
+++ vision_cv/script/trainer.py
@@ -5,6 +5,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
 from sklearn import cross_validation
 from sklearn.externals import joblib
 
@@ -25,17 +26,26 @@ def get_color_data():
 	purple = np.loadtxt(datapath + 'purple.txt')
 	red = np.loadtxt(datapath + 'red.txt')
 
-	n_samples, n_features = orange.shape
+	# Get number of samples of every dataset
+	oragnge_n_samples, _ = orange.shape
+	yellow_n_samples, _ = yellow.shape
+	lightgreen_n_samples, _ = lightgreen.shape
+	green_n_samples, _ = green.shape
+	lightblue_n_samples, _ = lightblue.shape
+	blue_n_samples, _ = blue.shape
+	purple_n_samples, _ = purple.shape
+	red_n_samples, _ = red.shape
+
 
 	# Generate targets
-	orange_y = np.zeros(n_samples)
-	yellow_y = np.ones(n_samples)
-	lightgreen_y = np.ones(n_samples) * 2
-	green_y = np.ones(n_samples) * 3
-	lightblue_y = np.ones(n_samples) * 4
-	blue_y = np.ones(n_samples) * 5
-	purple_y = np.ones(n_samples) * 6
-	red_y = np.ones(n_samples) * 7
+	orange_y = np.zeros(oragnge_n_samples)
+	yellow_y = np.ones(yellow_n_samples)
+	lightgreen_y = np.ones(lightgreen_n_samples) * 2
+	green_y = np.ones(green_n_samples) * 3
+	lightblue_y = np.ones(lightblue_n_samples) * 4
+	blue_y = np.ones(blue_n_samples) * 5
+	purple_y = np.ones(purple_n_samples) * 6
+	red_y = np.ones(red_n_samples) * 7
 
 	X = np.vstack((orange, yellow, lightgreen, green, lightblue, blue, purple, red))
 	y = np.hstack((orange_y, yellow_y, lightgreen_y, green_y, lightblue_y, blue_y, purple_y, red_y))
@@ -59,11 +69,12 @@ def get_material_data():
 	return (X, y)
 
 
+# X, y = get_color_data()
 X, y = get_material_data()
 
 clf = RandomForestClassifier(n_estimators=100)
-clf.fit(X, y)
-#scores = cross_validation.cross_val_score(clf, X, y)
-#print(scores)
+#clf.fit(X, y)
+scores = cross_validation.cross_val_score(clf, X, y)
+print(scores)
 
-joblib.dump(clf, datapath + 'material_clf.pkl')
+#joblib.dump(clf, datapath + 'color_clf.pkl')
